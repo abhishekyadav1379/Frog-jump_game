89734fb9daa80c93625c757230e54fcd36491f7d
Abhishek yadav <mrabhishek1379@gmail.com>
2020-08-31 12:39:37 +0530

    bug fix in river flow

--- script.js
+++ script.js
@@ -9,9 +9,11 @@ canvas.height = window.innerHeight;
 // global variable 
 var auto=0,xbg=0;   //background
 var xwater=0;  vel_water=4; //water
-function gameloop(){
-    ctx.drawImage(background,0,0,canvas.width,canvas.height);
 
+function gameloop(){
+    //background
+    ctx.drawImage(background, xbg ,0, canvas.width, canvas.height);
+    
     // water
     ctx.drawImage(water, xwater ,520, canvas.width, 140);
     xwater -= vel_water;

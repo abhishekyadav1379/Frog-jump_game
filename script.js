4794b276644721ea69cf78611c2d0daf43bd5f23
Abhishek yadav <mrabhishek1379@gmail.com>
2020-08-31 12:07:29 +0530

    river flow

--- script.js
+++ script.js
@@ -6,8 +6,20 @@ var ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
+// global variable 
+var auto=0,xbg=0;   //background
+var xwater=0;  vel_water=4; //water
 function gameloop(){
     ctx.drawImage(background,0,0,canvas.width,canvas.height);
+
+    // water
+    ctx.drawImage(water, xwater ,520, canvas.width, 140);
+    xwater -= vel_water;
+    ctx.drawImage(water, xwater+canvas.width ,520, canvas.width, 140);
+    if( xwater < (-1*canvas.width) ){
+        xwater = 0;
+    }
+
 }
 
 // fps

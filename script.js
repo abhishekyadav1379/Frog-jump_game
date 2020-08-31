641a0f71c2e0d2741c15d55a105e3d53930f7e11
IshitwaAgrawal <ishitwa2000@gmail.com>
2020-08-31 13:04:39 +0530

    added background movement

--- script.js
+++ script.js
@@ -6,13 +6,22 @@ var ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
+// global variables
+var auto=0,xbg=0;   //background
+
 function gameloop(){
-    ctx.drawImage(background,0,0,canvas.width,canvas.height);
+    //background
+    ctx.drawImage(background, xbg ,0, canvas.width, canvas.height);
+    xbg -= 1.5;
+    ctx.drawImage(background, xbg+canvas.width ,0, canvas.width, canvas.height);
+    if( xbg < (-1*canvas.width) ){
+        xbg = 0;
+    }
 }
 
 // fps
 function start() {
     window.requestAnimationFrame(start);  // refresh rate of screen 
-   gameloop();
+    gameloop();
 }
 start();
